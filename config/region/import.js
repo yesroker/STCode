1200039f640e5f70aa6720524d4b9cd9df81e999
yesroker <58024731+yesroker@users.noreply.github.com>
2025-11-16 05:42:29 +0800

    fix:去除多余文件

--- config/region/import.js
+++
@@ -1,4 +0,0 @@
-import {JSONPath} from 'jsonpath-plus'
-import * as _ from "lodash";
-import * as toastr from "toastr";
-import {TavernHelper,SillyTavern} from 'ST';
--- src/lib/lib.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  interface Window {
-    JSONPath: any; // <-- 解决 JSONPath 错误
-    STCode: any; // <-- 解决 STCode 错误
-  }
-}
-export {};
